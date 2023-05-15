ba278278a05e632d37711ffb51966bbd6db87b3c
Julio Rios <31230801+jsilverdev@users.noreply.github.com>
2023-05-15 18:15:33 -0500

    Obtain values from file and try to decode

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,35 @@
-import 'package:base64_properties_decoder/base64_properties_decoder.dart' as base64_properties_decoder;
+import 'dart:convert';
 
+import 'package:args/args.dart';
+import 'package:properties/properties.dart';
+import 'package:string_validator/string_validator.dart' as string_validator;
+
+String fileArgument = "file";
 void main(List<String> arguments) {
-  print('Hello world: ${base64_properties_decoder.calculate()}!');
+  final parser = ArgParser()..addOption(fileArgument, abbr: 'f');
+
+  ArgResults argResults = parser.parse(arguments);
+
+  final String filePath = argResults[fileArgument];
+
+  Properties p = Properties.fromFile(filePath);
+
+  for (var key in p.keys) {
+    final String value = p.get(key) ?? '';
+    if (!string_validator.isBase64(value)) continue;
+
+    final decodedValue = tryBase64Decode(value);
+    if(decodedValue == value) continue;
+    print("$key : $decodedValue");
+    // p.add(key, decodedValue);
+  }
+}
+
+String tryBase64Decode(value) {
+  try {
+    return utf8.decode(base64Decode(value));
+  } catch (e) {
+    print("Cant decode: $value");
+  }
+  return value;
 }
