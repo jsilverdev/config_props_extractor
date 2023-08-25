820561c02b61be9ee0ccf2413ea79c2adb2dbe93
Julio Rios <jrp8900@gmail.com>
2023-08-25 01:23:08 -0500

    Firsts tests fro shell service

--- lib/services/shell_service.dart
+++ lib/services/shell_service.dart
@@ -2,14 +2,17 @@ import 'dart:io';
 
 import '../exceptions/exceptions.dart';
 import 'package:process_run/process_run.dart';
+import 'package:path/path.dart' as path;
 
 class ShellService {
   Shell _shell;
 
   ShellService({Shell? shell}) : _shell = shell ?? Shell();
 
-  void checkExecutable(String executable) async {
-    final execLocation = whichSync(executable);
+  void checkExecutable(String executable) {
+    final execLocation = path.basename(executable) == executable
+        ? _shell.options.environment.whichSync(executable)
+        : null;
 
     if (execLocation == null) {
       throw ExecutableNotFoundInPathException(executable);
--- test/services/shell_service_test.dart
+++ test/services/shell_service_test.dart
@@ -1,7 +1,63 @@
+import 'package:config_props_extractor/exceptions/exceptions.dart';
+import 'package:config_props_extractor/services/shell_service.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:process_run/process_run.dart';
 import 'package:test/test.dart';
 
+class MockShell extends Mock implements Shell {}
+
 void main() {
-  test('shell service ...', () async {
-    // TODO: Implement test
+  late MockShell mockShell;
+
+  setUp(() {
+    mockShell = MockShell();
   });
+
+  group('which', () {
+    test(
+      'Should find executable in PATH',
+      () async {
+        // arrange
+
+        ShellEnvironment environment = ShellEnvironment.empty();
+        environment.paths.clear();
+        environment.paths.addAll(["test/_data/executables"]);
+
+        when(() => mockShell.options).thenReturn(
+          ShellOptions(
+            environment: environment,
+            includeParentEnvironment: false,
+          ),
+        );
+
+        // act
+        final shellService = ShellService(shell: mockShell);
+        shellService.checkExecutable("example");
+      },
+    );
+
+    test(
+      'Should throws if executable is not in PATH',
+      () async {
+        // arrange
+        when(() => mockShell.options).thenReturn(
+          ShellOptions(
+            environment: ShellEnvironment.empty()..paths.clear(),
+            includeParentEnvironment: false,
+          ),
+        );
+
+        final shellService = ShellService(shell: mockShell);
+
+        expect(
+          // act
+          () => shellService.checkExecutable("test"),
+          // assert
+          throwsA(isA<ExecutableNotFoundInPathException>()),
+        );
+      },
+    );
+  });
+
+  group('shell', () {});
 }
